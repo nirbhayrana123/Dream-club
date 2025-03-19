de4ff7d18302b65a7cb93d2716f9d449a34e7c00
nirbhayrana123 <nirbhydesign@gmail.com>
2025-03-19 22:50:32 +0530

    profile done

--- js/custom.js
+++ js/custom.js
@@ -279,3 +279,16 @@ document.querySelectorAll('.backintabs').forEach(button => {
   });
 });
 
+ ////////////////profile/////////////////
+
+ document.getElementById("fileInput").addEventListener("change", function(event) {
+  const file = event.target.files[0];
+  if (file) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+          document.getElementById("profilePic").src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+  }
+});
+/////////////////
